1b83a3e632424b0204d7e179731edfaba517b732
unknown <prateekruhela50@gmail.com>
2026-03-20 11:08:59 +0530

    [Prateek] Add. Solution for Linked List

---
+++ DSA/linked_list/DeleteNode.java
@@ -0,0 +1,21 @@
+
+class DeleteNode {
+
+    static Node delete(Node head, int key){
+
+        if(head==null) return null;
+
+        if(head.data==key) return head.next;
+
+        Node temp = head;
+
+        while(temp.next!=null && temp.next.data!=key){
+            temp = temp.next;
+        }
+
+        if(temp.next!=null)
+            temp.next = temp.next.next;
+
+        return head;
+    }
+}
